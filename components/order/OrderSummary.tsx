dc548f7611fc07fa7e06639a49ad202bcd7d3574
Emiliano Enriquez <enriquez.emiliano15@gmail.com>
2024-07-24 12:27:20 -0600

    Removiendo .env

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,7 +1,27 @@
+'use client'
+
+import { useStore } from "@/src/store"
+import ProductDetails from "./ProductDetails"
+
 export default function OrderSummary() {
+  const order = useStore(state => state.order)
+
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-54 lg:w-96 p-5">
-        <h1 className="text-4xl font-black text-center">Mi Pedido</h1>
+      <h1 className="text-4xl font-black text-center">Mi Pedido</h1>
+
+      {order.length === 0 ? (
+        <p className="text-center mt-10">Carrito Vacio</p>
+      ) : (
+        <div className="mt-5">
+          {order.map( item => (
+            <ProductDetails
+              key={item.id} 
+              item={item}
+            />
+          ))}
+        </div>
+      )}
     </aside>
   )
 }
