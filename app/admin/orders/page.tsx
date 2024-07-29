05f576e2c153d48416f78cb1b312b509b4dc1a97
Emiliano Enriquez <enriquez.emiliano15@gmail.com>
2024-07-28 22:19:05 -0600

    Agregando SWR para actualizr las ordenes nuevas

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,34 +1,27 @@
-import OrderCard from "@/components/order/OrderCard";
-import Heading from "@/components/ui/Heading";
-import { prisma } from "@/src/lib/prisma";
+'use client'
 
-async function getPendingOrders() {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: false
-    },
-    include: {
-      orderProducts: {
-        include: {
-          product: true
-        }
-      }
-    }
+import useSWR from "swr"
+import OrderCard from "@/components/order/OrderCard"
+import Heading from "@/components/ui/Heading"
+import { OrderWithProducts } from "@/src/types"
+
+export default function page() {
+  const url = '/admin/orders/api'
+  const fetcher = () => fetch(url).then( res => res.json() ).then( data => data)
+  const { data, error, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, { 
+    refreshInterval: 60000,
+    revalidateOnFocus: false
   })
 
-  return orders
-}
+  if(isLoading) return 'Cargando...'
 
-export default async function page() {
-  const orders = await getPendingOrders()
-
-  return (
+  if(data) return (
     <div>
         <Heading>Administrar Ordenes</Heading>
 
-        {orders.length ? (
+        {data.length ? (
           <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-            {orders.map( order => (
+            {data.map( order => (
               <OrderCard
                 key={order.id}
                 order={order}
